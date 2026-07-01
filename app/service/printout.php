af9ff3be4ce1b4d436c78840da45d7a20bacd3c2
ahmadreza0101 <196525791+ahmadreza0101@users.noreply.github.com>
2026-07-01 11:55:28 +0700

    Optimized Print

--- app/service/printout.php
+++ app/service/printout.php
@@ -10,7 +10,8 @@ if (empty($id) && empty($kelas)) {
     die("ID atau kelas harus diberikan!");
 }
 
-// Tentukan orientation: landscape jika menampilkan banyak data (kelas), portrait jika detail single
+$canUseImages = extension_loaded('gd') || extension_loaded('imagick');
+
 $orientation = !empty($kelas) ? 'L' : 'P';
 
 $pdf = new TCPDF($orientation, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
@@ -29,7 +30,6 @@ $pdf->SetFont('helvetica', '', !empty($kelas) ? 10 : 12);
 $html = '';
 
 if (!empty($kelas)) {
-    // Perbaiki query: hilangkan is_active karena kolom tersebut mungkin tidak ada
     $stmt = mysqli_prepare($koneksi, "SELECT * FROM tb_jadwal WHERE kelas = ? ORDER BY tanggal_ujian ASC, waktu_mulai ASC");
     mysqli_stmt_bind_param($stmt, 's', $kelas);
     mysqli_stmt_execute($stmt);
@@ -37,16 +37,17 @@ if (!empty($kelas)) {
     
     $html .= '<h1 style="text-align: center; margin-bottom: 20px; color: #2563eb;">Jadwal Ujian Kelas ' . htmlspecialchars($kelas) . '</h1>';
     $html .= '<table border="1" cellpadding="6" cellspacing="0" style="width: 100%; border-collapse: collapse;">';
+    $gambarColumnHeader = $canUseImages ? '<th style="text-align: center; width: 10%;">Gambar</th>' : '';
     $html .= '<thead>
                 <tr style="background-color: #2563eb; color: white;">
                     <th style="text-align: center; width: 5%;">No</th>
-                    <th style="text-align: center; width: 10%;">Gambar</th>
-                    <th style="text-align: center; width: 10%;">Tanggal</th>
-                    <th style="text-align: center; width: 13%;">Waktu</th>
-                    <th style="text-align: center; width: 20%;">Mata Kuliah</th>
+                    ' . $gambarColumnHeader . '
+                    <th style="text-align: center; width: ' . ($canUseImages ? '10%' : '11.1%') . ';">Tanggal</th>
+                    <th style="text-align: center; width: ' . ($canUseImages ? '13%' : '14.4%') . ';">Waktu</th>
+                    <th style="text-align: center; width: ' . ($canUseImages ? '20%' : '22.2%') . ';">Mata Kuliah</th>
                     <th style="text-align: center; width: 5%;">SKS</th>
-                    <th style="text-align: center; width: 20%;">Dosen</th>
-                    <th style="text-align: center; width: 17%;">Ruangan</th>
+                    <th style="text-align: center; width: ' . ($canUseImages ? '20%' : '22.2%') . ';">Dosen</th>
+                    <th style="text-align: center; width: ' . ($canUseImages ? '17%' : '18.9%') . ';">Ruangan</th>
                 </tr>
               </thead>
               <tbody>';
@@ -55,26 +56,23 @@ if (!empty($kelas)) {
     while ($row = mysqli_fetch_assoc($result)) {
         $bgColor = ($no % 2 == 0) ? '#f8fafc' : '#ffffff';
         $gambarHtml = '';
-        if (!empty($row['gambar'])) {
+        if ($canUseImages && !empty($row['gambar'])) {
             $gambarHtml = '<td style="text-align: center; width: 10%;"><img src="' . htmlspecialchars($row['gambar']) . '" style="width: 50px; height: 50px; object-fit: cover;" /></td>';
-        } else {
-            $gambarHtml = '<td style="text-align: center; width: 10%;">-</td>';
         }
         $html .= '<tr style="background-color: ' . $bgColor . ';">
                     <td style="text-align: center; width: 5%;">' . $no++ . '</td>
                     ' . $gambarHtml . '
-                    <td style="text-align: center; width: 10%;">' . date('d/m/Y', strtotime($row['tanggal_ujian'])) . '</td>
-                    <td style="text-align: center; width: 13%;">' . date('H:i', strtotime($row['waktu_mulai'])) . ' - ' . date('H:i', strtotime($row['waktu_selesai'])) . '</td>
-                    <td style="width: 20%;">' . htmlspecialchars($row['mata_kuliah']) . '</td>
+                    <td style="text-align: center; width: ' . ($canUseImages ? '10%' : '11.1%') . ';">' . date('d/m/Y', strtotime($row['tanggal_ujian'])) . '</td>
+                    <td style="text-align: center; width: ' . ($canUseImages ? '13%' : '14.4%') . ';">' . date('H:i', strtotime($row['waktu_mulai'])) . ' - ' . date('H:i', strtotime($row['waktu_selesai'])) . '</td>
+                    <td style="width: ' . ($canUseImages ? '20%' : '22.2%') . ';">' . htmlspecialchars($row['mata_kuliah']) . '</td>
                     <td style="text-align: center; width: 5%;">' . htmlspecialchars($row['sks'] ?? 3) . '</td>
-                    <td style="width: 20%;">' . htmlspecialchars($row['dosen']) . '</td>
-                    <td style="width: 17%;">' . htmlspecialchars($row['ruangan']) . '</td>
+                    <td style="width: ' . ($canUseImages ? '20%' : '22.2%') . ';">' . htmlspecialchars($row['dosen']) . '</td>
+                    <td style="width: ' . ($canUseImages ? '17%' : '18.9%') . ';">' . htmlspecialchars($row['ruangan']) . '</td>
                 </tr>';
     }
     
     $html .= '</tbody></table>';
 } else {
-    // Perbaiki query: hilangkan is_active
     $stmt = mysqli_prepare($koneksi, "SELECT * FROM tb_jadwal WHERE id = ?");
     mysqli_stmt_bind_param($stmt, 'i', $id);
     mysqli_stmt_execute($stmt);
@@ -89,14 +87,14 @@ if (!empty($kelas)) {
     $html .= '<table style="width: 100%; border-collapse: collapse;">';
     $html .= '<tr>';
     
-    if (!empty($row['gambar'])) {
+    if ($canUseImages && !empty($row['gambar'])) {
         $html .= '<td style="width: 220px; vertical-align: top;">';
         $html .= '<img src="' . htmlspecialchars($row['gambar']) . '" 
                          style="width: 200px; height: auto; border-radius: 10px;" />';
         $html .= '</td>';
     }
     
-    $html .= '<td style="vertical-align: top; padding-left: 20px;">';
+    $html .= '<td style="vertical-align: top; padding-left: ' . ($canUseImages && !empty($row['gambar']) ? '20px' : '0') . ';">';
     $html .= '<h2 style="color: #2563eb; margin-top: 0; margin-bottom: 20px;">' . htmlspecialchars($row['mata_kuliah']) . '</h2>';
     $html .= '<table style="width: 100%; border-collapse: collapse;">
                 <tr>
